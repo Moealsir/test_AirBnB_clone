063dd507714106870bac40d0c3a65132988c3ea3
Moealsir <mohamedwdalsir@gmail.com>
2024-02-06 21:02:46 +0300

    destroy

--- console.py
+++ console.py
@@ -2,32 +2,33 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+
 class HBNBCommand(cmd.Cmd):
-    """command line interface"""
-    
+    """Command line interface"""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.prompt = '(hbnb)'
-        self.classes = ["BaseModel"]
+        self.prompt = '(hbnb) '
+        self.classes = ["BaseModel", "User"]
         self.objects = storage.all()
         storage.reload()
-        
+
     def emptyline(self):
         pass
-    
+
     def do_EOF(self, line):
         if len(line) == 0:
             return True
-    
+
     def help_quit(self):
         print("Quit command to exit the program\n")
-    
+
     def do_quit(self, line):
         if len(line) == 0:
             return True
-    
+
     def do_create(self, line):
-        """function to create a new instance of a class"""
+        """Function to create a new instance of a class"""
         args = line.split()
         if not args:
             print("** class name missing **")
@@ -37,11 +38,11 @@ class HBNBCommand(cmd.Cmd):
             return
         else:
             new_instance = BaseModel()
-            new_instance.save()
             print(new_instance.id)
-            
+            new_instance.save()
+
     def do_show(self, line):
-        """function to show an instance of a class"""
+        """Function to show an instance of a class"""
         args = line.split()
         if not args:
             print("** class name missing **")
@@ -53,13 +54,31 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
         else:
-            
             key = "{} {}".format(args[0], args[1])
             if key in self.objects.keys():
                 print(self.objects[key])
             else:
                 print("** no instance found **")
 
-    
+    def do_destroy(self, line):
+        """Function to destroy an instance of a class"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        elif len(args) == 1:
+            print("** instance id missing **")
+            return
+        else:
+            key = "{} {}".format(args[0], args[1])
+            if key in self.objects:
+                del self.objects[key]
+                storage.save()  # Save after deleting the instance
+            else:
+                print("** no instance found **")
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
