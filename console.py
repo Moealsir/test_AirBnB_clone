1fede5bdd366905ac4c8de2c5d9e1d860d1eaca6
Moealsir <mohamedwdalsir@gmail.com>
2024-02-06 20:16:25 +0300

    create && show

---
+++ console.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+import cmd
+from models.base_model import BaseModel
+from models import storage
+class HBNBCommand(cmd.Cmd):
+    """command line interface"""
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.prompt = '(hbnb)'
+        self.classes = ["BaseModel"]
+        storage.reload()
+        
+    def emptyline(self):
+        pass
+    
+    def do_EOF(self, line):
+        if len(line) == 0:
+            return True
+    
+    def help_quit(self):
+        print("Quit command to exit the program\n")
+    
+    def do_quit(self, line):
+        if len(line) == 0:
+            return True
+    
+    def do_create(self, line):
+        """function to create a new instance of a class"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+            
+    def do_show(self, line):
+        """function to show an instance of a class"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        elif len(args) == 1:
+            print("** instance id missing **")
+            return
+        else:
+            objects = storage.all()
+            key = "{} {}".format(args[0], args[1])
+            if key in objects.keys():
+                print(objects[key])
+            else:
+                print("** no instance found **")
+            # obj = storage.all().get(obj_id)
+            # if obj:
+            #     print(obj)
+            # else:
+            #     print("** no instance found **")
+        
+    
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -8,16 +8,18 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from models.engine import file_storage
 from models.base_model import BaseModel
 from models import storage
-
 all_objs = storage.all()
+opjs = []
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
-    print(obj)
+    opjs.append(obj)
 
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
+print(opjs)
+
+# print("-- Create a new object --")
+# my_model = BaseModel()
+# my_model.name = "My_First_Model"
+# my_model.my_number = 89
+# my_model.save()
+# print(my_model)
