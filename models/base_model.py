643b90e77b6f79d82df02c5ef8e284b5b05f892e
Moealsir <mohamedwdalsir@gmail.com>
2024-02-06 11:47:19 +0300

    task4

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,22 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """
-        initialize BaseModel instance.
+        Constructor method to initialize BaseModel instance.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """print string
@@ -31,8 +42,7 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """>>
-
+        """
         Return a dictionary representation of an object.
 
         Returns:
