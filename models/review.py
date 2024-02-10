a4f6215ecac841eb13ff3e200b1284c1db1ee681
Moealsir <mohamedwdalsir@gmail.com>
2024-02-10 22:16:31 +0300

    test

--- models/review.py
+++ models/review.py
@@ -6,9 +6,6 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """Review class"""
 
-    def __init__(self, *args, **kwargs):
-        """initialize attributes"""
-        super().__init__(*args, **kwargs)
-        self.place_id = kwargs.get("place_id", "")
-        self.user_id = kwargs.get("user_id", "")
-        self.text = kwargs.get("user_id", "")
+    place_id = ""
+    user_id = ""
+    text = ""
