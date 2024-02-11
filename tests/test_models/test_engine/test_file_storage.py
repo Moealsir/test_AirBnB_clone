6b2298245554cc9d5399a41e25fe825f74986dea
Moealsir <mohamedwdalsir@gmail.com>
2024-02-11 15:38:37 +0300

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,16 +18,5 @@ sys.path.append('../../')
 class TestFileStorage(unittest.TestCase):
 
 
-    @patch("builtins.open", new_callable=mock_open, read_data='{"BaseModel.1234": {"__class__": "BaseModel", "id": "1234", "created_at": "2021-11-02T14:15:22", "updated_at": "2021-11-02T14:15:22"}}')
-    def test_reload_with_data(self, mock_open):
-        self.storage.reload()
-        self.assertTrue(self.storage._FileStorage__objects)
-        key = "BaseModel.1234"
-        self.assertIn(key, self.storage._FileStorage__objects)
-        obj = self.storage._FileStorage__objects[key]
-        self.assertIsInstance(obj, BaseModel)
-        self.assertEqual(obj.id, "1234")
-
-
 if __name__ == '__main__':
     unittest.main()
